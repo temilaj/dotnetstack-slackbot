b0f9ed4ce27742653f28ea56fbb744904ee18452
bolorundurowb <ogatimo@gmail.com>
2016-09-25 15:50:22 +0100

    travis

--- local-coverage.sh
+++ local-coverage.sh
@@ -1,4 +1,4 @@
-mono --debug --profile=monocov:outfile=monocovCoverage.cov,+dotnetstack-slackbot ./packages/NUnit.ConsoleRunner.3.4.1/tools/nunit3-console.exe --process=Single ./dotnetstack-slackbot-Tests/bin/Release/dotnetstack-slackbot-Tests.dll
+mono --debug --profile=monocov:outfile=monocovCoverage.cov,+dotnetstack-slackbot ./packages/NUnit.Console.3.0.0/tools/nunit3-console.exe --process=Single ./dotnetstack-slackbot-Tests/bin/Debug/dotnetstack-slackbot-Tests.dll
 monocov --export-xml=monocovCoverage monocovCoverage.cov
 cat monocovCoverage.cov
 ls monocovCoverage
